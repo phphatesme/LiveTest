a31cc0b64f8a15e1c46f64369845a48417480cb9
Mike Lohmann <mikelohmann@192.168.20.2>
2011-02-08 08:00:35 +0100

    Added new tests for parameters

--- test/unit/LiveTest/TestRun/TestTest.php
+++ test/unit/LiveTest/TestRun/TestTest.php
@@ -30,9 +30,33 @@ class TestTest extends PHPUnit_Framework_TestCase
     $this->assertEquals('testName', $this->test->getName());
   }
   
-  public function getParameter()
+  public function testGetParameter()
   {
     $this->assertEquals(true, is_string($this->test->getName()));
     $this->assertEquals('testName', $this->test->getName());
+  }
+  
+   /**
+     * @expectedException Base\Cli\WrongTypeException
+     */
+  public function testConstructorParameterClassException()
+  {
+    new Test('testName', 'ClassMockup', array('properties'));
+  }
+  
+ /**
+     * @expectedException Base\Cli\WrongTypeException
+     */
+  public function testConstructorParameterNameException()
+  {
+    new Test(1, new ClassMockup(), array('properties'));
+  }
+  
+  /**
+     * @expectedException PHPUnit_Framework_Error
+     */
+  public function testConstructorParameterPropertiesException()
+  {
+    new Test('testName', new ClassMockup(), 'properties');
   }
 }
