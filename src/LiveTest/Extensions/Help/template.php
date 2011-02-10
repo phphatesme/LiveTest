8e2b473a3e24627f5869261a1b4be1bd3b1421be
Nils Langner <langner@plan08.de>
2011-02-10 13:38:13 +0100

    Removed template file for help extension

--- src/LiveTest/Extensions/Help/template.php
+++
@@ -1,9 +0,0 @@
-
-Usage: LiveTest --testsuite testsuite.yml [--config config.yml]
-
-  --help              Prints this usage information.
-  
-  --testsuite <file>  Defines the testsuite to be run.
-  --config <file>     Defines the config file.
-  
-  --debug             Show all uncaught exceptions.
