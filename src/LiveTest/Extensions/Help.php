8752cb0d3eab958d86f1b2b592ff812c2b099d3e
Nils Langner <langner@plan08.de>
2011-02-12 01:54:03 +0800

    Added PHPDoc for Help Extension

--- src/LiveTest/Extensions/Help.php
+++ src/LiveTest/Extensions/Help.php
@@ -11,17 +11,42 @@ use LiveTest\TestRun\Properties;
 use LiveTest\TestRun\Test;
 use LiveTest\TestRun\Result\Result;
 
+/**
+ * This extension is used to print the help content if the --help argument is set
+ * 
+ * @author Nils Langner
+ */
+
 class Help implements Extension
 {
+  /**
+   * The command line arguments
+   * 
+   * @var string[]
+   */
   private $arguments;
   
+  /**
+   * This template that contains the help text
+   * @var string The filename. Relative to __DIR__. 
+   */
   private $template = 'Help/template.tpl'; 
   
+  /**
+   * @see LiveTest\Extensions\Extension::__construct()
+   */
   public function __construct($runId, \Zend_Config $config = null, $arguments = null)
   {
     $this->arguments = $arguments;
   }
   
+  /**
+   * This function echoes the global help if the --help command line argument is set
+   * 
+   * @see LiveTest\Extensions\Extension::preRun()
+   * 
+   * @param Properties $properties
+   */
   public function preRun(Properties $properties)
   {
     if (array_key_exists('help', $this->arguments) || count( $this->arguments ) == 0)
@@ -32,14 +57,30 @@ class Help implements Extension
     return true;
   }
   
+  /**
+   * not used
+   * 
+   * @param ConnectionStatus $status
+   */
   public function handleConnectionStatus(ConnectionStatus $status)
   {
   }
   
+  /**
+   * not used
+   * 
+   * @param Result $result
+   * @param Response $response
+   */
   public function handleResult(Result $result, Response $response)
   {
   }
   
+  /**
+   * not used
+   * 
+   * @param Information $information
+   */
   public function postRun(Information $information)
   {
   }
