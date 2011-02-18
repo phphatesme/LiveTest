77521453750c221df43fef4447e452466b7acb29
Nils Langner <langner@plan08.de>
2011-02-18 11:01:47 +0100

    Added new listener test cases

---
+++ test/Unit/LiveTest/Listener/SleepTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Test\Unit\LiveTest\Extensions;
+
+use Annovent\Event\Dispatcher;
+
+use Base\Http\Response;
+
+use Base\Timer\Timer;
+
+use LiveTest\Listener\Sleep;
+
+use Base\Www\Uri;
+use Base\Http\ConnectionStatus;
+
+use LiveTest\TestRun\Test;
+use LiveTest\TestRun\Result\Result;
+
+class SleepTest extends \PHPUnit_Framework_TestCase
+{
+  private $sleepTime = 1;
+
+  private $listener;
+
+  public function testHandleResult()
+  {
+    $this->listener = new Sleep('', new Dispatcher());
+    $this->listener->init(1);
+
+    $test = new Test('', '', new \Zend_Config(array()));
+    $response = new Response(new \Zend_Http_Response(200, array()));
+
+    $timer = new Timer();
+    $this->listener->handleConnectionStatus( new ConnectionStatus(ConnectionStatus::SUCCESS, new Uri( 'http://www.example.com')));
+    $this->assertGreaterThanOrEqual($this->sleepTime, $timer->stop());
+  }
+}
