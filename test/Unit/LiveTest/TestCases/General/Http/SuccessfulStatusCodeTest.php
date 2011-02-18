684b266fcfddbfc6479d8eb50c8fb9630153510b
Nils Langner <langner@plan08.de>
2011-02-18 13:36:20 +0100

    Added unit tests for http test cases

---
+++ test/Unit/LiveTest/TestCases/General/Http/SuccessfulStatusCodeTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Test\Unit\LiveTest\TestCases\General\Http;
+
+use Base\Www\Uri;
+
+use Base\Http\Response\Zend;
+
+use LiveTest\TestCase\General\Http\ExpectedStatusCode;
+
+class SuccessfulStatusCodeTest extends \PHPUnit_Framework_TestCase
+{
+  public function testNegativeTest()
+  {
+    $testCase = new ExpectedStatusCode();
+    $testCase->init(400);
+
+    $response = $this->getMock('\Base\Http\Response\Response', array('getStatus', 'getBody'));
+    $response->expects($this->any())
+                 ->method('getStatus')
+                 ->will($this->returnValue(500));
+
+    $this->setExpectedException('LiveTest\TestCase\Exception');
+    $testCase->test($response, new Uri('http://www.example.com'));
+  }
+
+  public function testPositiveTest()
+  {
+    $testCase = new ExpectedStatusCode();
+    $testCase->init(400);
+
+    $response = $this->getMock('\Base\Http\Response\Response', array('getStatus', 'getBody'));
+    $response->expects($this->any())
+                 ->method('getStatus')
+                 ->will($this->returnValue(400));
+
+    $testCase->test($response, new Uri('http://www.example.com'));
+  }
+}
