9532402cd52c23794bdbfb07c2aaf3eabc744dd9
Nils Langner <langner@plan08.de>
2011-02-22 21:00:47 +0100

    Added PHPDoc to Report/Format

--- src/LiveTest/Report/Format/Format.php
+++ src/LiveTest/Report/Format/Format.php
@@ -7,5 +7,14 @@ use LiveTest\TestRun\Result\ResultSet;
 
 interface Format
 {
+  /**
+   * Format the given test run results and return a string.
+   * 
+   * @param ResultSet $set
+   * @param array $connectionStatuses
+   * @param Information $information
+   * 
+   * @return string
+   */
   public function formatSet(ResultSet $set, array $connectionStatuses, Information $information);
 }
