103f539be32b075ab9035bc8d348a3e46a69bf18
Sven Paulus <sven@karlsruhe.org>
2011-02-23 17:45:48 +0100

    add license boiler plate to all php files

--- src/LiveTest/TestRun/Properties.php
+++ src/LiveTest/TestRun/Properties.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of the LiveTest package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 // @fixme Diese Datei muss gerefactored werden!!!
 //        - Harte Kopplung an yaml
 //        - Lange Methoden
--- src/LiveTest/TestRun/Result/Result.php
+++ src/LiveTest/TestRun/Result/Result.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of the LiveTest package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace LiveTest\TestRun\Result;
 
 use Base\Www\Uri;
