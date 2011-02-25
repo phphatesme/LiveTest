fdd7f47dfed2102b7374bad61e6be016c8b79802
Nils Langner <langner@plan08.de>
2011-02-25 12:51:06 +0100

    Added Headlines to SimpleList writer

--- src/LiveTest/Report/Format/SimpleList.php
+++ src/LiveTest/Report/Format/SimpleList.php
@@ -9,54 +9,76 @@
 
 namespace LiveTest\Report\Format;
 
+use Base\Http\ConnectionStatus;
+
 use LiveTest\TestRun\Information;
 
 use LiveTest\TestRun\Result\ResultSet;
 use LiveTest\TestRun\Result\Result;
 
 /**
- * This format converts the results into a simple text list. It is used to print the 
+ * This format converts the results into a simple text list. It is used to print the
  * when running on command line.
- * 
+ *
  * @author Nils Langner
  */
 class SimpleList implements Format
 {
   /**
    * Formats the results into a simple list.
-   * 
+   *
    * @param ResultSet $set
    * @param array $connectionStatuses
    * @param Information $information
-   * 
+   *
    * @return string
    */
   public function formatSet(ResultSet $set, array $connectionStatuses, Information $information)
   {
     $text = '';
-    foreach ( $set->getResults() as $result )
+
+    if (count($connectionStatuses) > 0)
     {
-      $test = $result->getTest();
-      /* @var $test Test*/
-      $text .= '     Url        :  ' . $result->getUri()->toString() . "\n";
-      $text .= '     Test       :  ' . $test->getName() . "\n";
-      $text .= '     Test Class :  ' . $test->getClassName() . "\n";
-      switch ($result->getStatus())
+      $text .= "     Connection Statuses: \n\n";
+
+      foreach ($connectionStatuses as $connectionStatus)
       {
-        case Result::STATUS_SUCCESS :
-          $text .= '     Status     :  Success' . "\n";
-          break;
-        case Result::STATUS_FAILED :
-          $text .= '     Status     :  Failed' . "\n";
-          $text .= '     Message    :  ' . $result->getMessage() . "\n";
-          break;
-        case Result::STATUS_ERROR :
-          $text .= '     Status     :  Error' . "\n";
-          $text .= '     Message    :  ' . $result->getMessage() . "\n";
-          break;
-        default :
+        if ($connectionStatus->getType() == ConnectionStatus::ERROR)
+        {
+          echo "       Url     : " . $connectionStatus->getUri()->toString() . "\n";
+          echo "       Message : " . $connectionStatus->getMessage() . "\n\n";
+        }
       }
-      $text .= "\n";
+    }
+
+    if ($set->getResultCount() > 0)
+    {
+      $text .= "     Result Statuses:\n\n";
+
+      foreach ($set->getResults() as $result)
+      {
+        $test = $result->getTest();
+        /* @var $test Test*/
+        $text .= '     Url        :  ' . $result->getUri()->toString() . "\n";
+        $text .= '     Test       :  ' . $test->getName() . "\n";
+        $text .= '     Test Class :  ' . $test->getClassName() . "\n";
+        switch ($result->getStatus())
+        {
+          case Result::STATUS_SUCCESS :
+            $text .= '     Status     :  Success' . "\n";
+            break;
+          case Result::STATUS_FAILED :
+            $text .= '     Status     :  Failed' . "\n";
+            $text .= '     Message    :  ' . $result->getMessage() . "\n";
+            break;
+          case Result::STATUS_ERROR :
+            $text .= '     Status     :  Error' . "\n";
+            $text .= '     Message    :  ' . $result->getMessage() . "\n";
+            break;
+          default :
+        }
+        $text .= "\n";
+      }
     }
     return $text;
   }
--- test/Unit/LiveTest/Report/Format/SimpleListTest.php
+++ test/Unit/LiveTest/Report/Format/SimpleListTest.php
@@ -22,7 +22,8 @@ class SimpleListTest extends FormatTest
   {
     $formattedText = $this->getStandardFormattedContent();
 
-    $expected = "     Url        :  http://www.example.com\n     Test       :  TestName\n     Test Class :  TestClass\n     Status     :  Success\n\n".
+    $expected = "     Result Statuses:\n\n".
+                "     Url        :  http://www.example.com\n     Test       :  TestName\n     Test Class :  TestClass\n     Status     :  Success\n\n".
                 "     Url        :  http://www.example.com\n     Test       :  TestName\n     Test Class :  TestClass\n     Status     :  Failed\n     Message    :  Failed Message\n\n".
                 "     Url        :  http://www.example.com\n     Test       :  TestName\n     Test Class :  TestClass\n     Status     :  Error\n     Message    :  Error Message\n\n";
 
