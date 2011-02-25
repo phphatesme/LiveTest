ff6d1552d075c94ba4dafc3020c678501ec38fbf
Nils Langner <langner@plan08.de>
2011-02-25 12:52:12 +0100

    Added new automatic loader for tags

--- src/LiveTest/Config/Parser.php
+++ src/LiveTest/Config/Parser.php
@@ -4,30 +4,13 @@ namespace LiveTest\Config;
 
 class Parser
 {
-  private $configArray;
-  private $config;
-  private $registeredTags;
-
-  public function __construct()
-  {
-    // @todo This should be done automaticly
-    $this->registeredTags = array('TestSuite' => 'LiveTest\Config\Tags\TestSuite',
-                                  'TestCases' => 'LiveTest\Config\Tags\TestCases',
-    							  'TestCase'  => 'LiveTest\Config\Tags\TestCase',
-    							  'Pages'       => 'LiveTest\Config\Tags\Pages',
-    							  'IncludePages' => 'LiveTest\Config\Tags\IncludePages',
-    							  'ExcludePages' => 'LiveTest\Config\Tags\ExcludePages',
-    							  'PageLists' => 'LiveTest\Config\Tags\PageLists',
-    							  'PageFiles' => 'LiveTest\Config\Tags\PageFiles' );
-  }
-
   public function parse(array $configArray, Config $config)
   {
     foreach ($configArray as $configTag => $value)
     {
-      if (array_key_exists($configTag, $this->registeredTags))
+      $tagClassName = $this->getTagClassName($configTag);
+      if (class_exists($tagClassName))
       {
-        $tagClassName = $this->registeredTags[$configTag];
         $tag = new $tagClassName($value, $config, $this);
         $tag->process();
       }
@@ -38,5 +21,18 @@ class Parser
     }
 
     return $config;
+  }
+
+  private function getTagClassName($tag)
+  {
+    if (strpos($tag, '\\') === false)
+    {
+      $className = 'LiveTest\\Config\\Tags\\' . $tag;
+    }
+    else
+    {
+      $className = $tag;
+    }
+    return $className;
   }
 }
