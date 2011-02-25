36eff3e2ccefd5ac091e1ab896ddaebf66503f93
Nils Langner <langner@plan08.de>
2011-02-25 14:43:05 +0100

    Code beautification

--- src/LiveTest/Config/Tags/PageFiles.php
+++ src/LiveTest/Config/Tags/PageFiles.php
@@ -9,7 +9,6 @@ class PageFiles extends Base
   protected function doProcess(Config $config, array $parameters)
   {
     $config->doNotInherit();
-
     foreach ($parameters as $file)
     {
       $config->includePages(file($config->getBaseDir() . '/' . $file));
