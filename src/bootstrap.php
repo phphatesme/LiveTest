fcf88f1e57df99567ca504bc2734d5589873c4ea
Nils Langner <langner@plan08.de>
2011-02-27 17:36:04 +0100

    Clean up and documentation of bootstrapper

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,17 +1,31 @@
 <?php
 
-// @todo autoload vereinheitlichen
+/* -----------------------------------------------------------------------
+ * Include 3rd party dependecies
+ * ----------------------------------------------------------------------- */
+
+// include the sfYaml parser of the symfony components
 include_once 'lib/SymfonyComponents/Yaml/sfYaml.php';
+
+// include the base library
 include_once 'lib/Base/bootstrap.php';
 
+// include the Annovent event despatcher
 include_once 'lib/Annovent/bootstrap.php';
 
-include_once 'LiveTest/functions.php';
-
+// include zend framework
 set_include_path(__DIR__ . '/lib' . PATH_SEPARATOR . get_include_path());
 require_once 'Zend/Loader/Autoloader.php';
 Zend_Loader_Autoloader::getInstance();
 
+/* -----------------------------------------------------------------------
+ * Include LiveTest classes and functions
+ * ----------------------------------------------------------------------- */
+
+// include the live test functions
+include_once 'LiveTest/functions.php';
+
+// register livetest autoloader
 function LiveTest_Autoload($classname)
 {
   $currentDir = __DIR__;
