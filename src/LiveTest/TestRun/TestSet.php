afa630dee0ccb598431df160c6e4db5390abdbc8
Nils Langner <langner@plan08.de>
2011-02-27 15:18:25 +0100

    Added PHPDoc and TypeHint for TestSet.php

--- src/LiveTest/TestRun/TestSet.php
+++ src/LiveTest/TestRun/TestSet.php
@@ -9,34 +9,68 @@
 
 namespace LiveTest\TestRun;
 
+use LiveTest\TestRun\Test;
 use Base\Www\Uri;
 
+/**
+ * A test set contains a number of tests that will be run on a spefific uri.
+ *
+ * @author Nils Langner
+ */
 class TestSet
 {
+  /**
+   * The uri for this test set
+   * @var Uri
+   */
   private $uri;
 
-  private $tests = array();
+  /**
+   * The tests
+   * @var Test[]
+   */
+  private $tests = array ();
 
   public function __construct(Uri $uri)
   {
     $this->uri = $uri;
   }
 
+  /**
+   * Returns the uri of this test set.
+   *
+   * @return Uri
+   */
   public function getUri()
   {
     return $this->uri;
   }
 
-  public function addTest($test)
+  /**
+   * Add a test to this set
+   *
+   * @param Test $test
+   */
+  public function addTest(Test $test)
   {
     $this->tests[] = $test;
   }
 
-  public function getTestCount( )
+  /**
+   * Returns the number of tests
+   *
+   * @return int
+   */
+  public function getTestCount()
   {
     return count($this->tests);
   }
 
+  /**
+   * Returns a set of tests
+   *
+   * @return Test[]
+   */
   public function getTests()
   {
     return $this->tests;
