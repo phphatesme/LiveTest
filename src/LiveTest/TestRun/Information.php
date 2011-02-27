50639794c41b6a4576b5ae79792328ce1601065c
Nils Langner <langner@plan08.de>
2011-02-27 15:27:49 +0100

    Added PHPDoc for Information.php

--- src/LiveTest/TestRun/Information.php
+++ src/LiveTest/TestRun/Information.php
@@ -11,22 +11,46 @@ namespace LiveTest\TestRun;
 
 use Base\Www\Uri;
 
+/**
+ * This class contains all information about a test run.
+ *
+ * @author Nils Langner
+ */
 class Information
 {
+  /**
+   * The duration of the test run in seconds
+   * @var int
+   */
   private $duration;
+
+  /**
+   * The default domain the test run was run against.
+   * @var Uri
+   */
   private $defaultDomain;
-  
+
   public function __construct($duration, Uri $defaultDomain )
   {
     $this->duration = $duration;
     $this->defaultDomain = $defaultDomain;
   }
-  
+
+  /**
+   * Returns the default domain.
+   *
+   * @return Uri
+   */
   public function getDefaultDomain( )
   {
     return $this->defaultDomain;
   }
-  
+
+  /**
+   * Returns the duration of the test run.
+   *
+   * @return int
+   */
   public function getDuration()
   {
     return $this->duration;
