a7e09e2ecd13a3feba457ad3bc3bf8100d17902b
Nils Langner <langner@plan08.de>
2011-02-28 19:56:30 +0100

    Documented Dispatcher.php

--- src/LiveTest/Event/Dispatcher.php
+++ src/LiveTest/Event/Dispatcher.php
@@ -6,8 +6,20 @@ use LiveTest\Config\ConfigConfig;
 
 use Annovent\Event\Dispatcher as AnnoventDispatcher;
 
+/**
+ * This dispatcher is a standard Annovent dispatcher with the possibility to register
+ * listeners using a ConfigConfig object.
+ *
+ * @author Nils Langner
+ */
 class Dispatcher extends AnnoventDispatcher
 {
+  /**
+   * This function is used to register listeners using a global configuration file
+   *
+   * @param ConfigConfig $config
+   * @param string $runId
+   */
   public function registerListenersByConfig(ConfigConfig $config, $runId)
   {
     foreach ( $config->getListeners() as $listener )
