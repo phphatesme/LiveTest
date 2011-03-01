127b1400cd33b5be2b6d94e113d99729cab0c3fe
Nils Langner <langner@plan08.de>
2011-03-01 09:28:16 +0100

    Documented TestSuite config

--- src/LiveTest/Config/TestSuite.php
+++ src/LiveTest/Config/TestSuite.php
@@ -1,29 +1,87 @@
 <?php
 
+/*
+ * This file is part of the LiveTest package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace LiveTest\Config;
+
+/**
+ * This class contains all information about the listeners and the default domain. It is used to
+ * represent the config yaml and is filled by the tag system and the parse.
+ *
+ * @author Nils Langner
+ */
 
 class TestSuite implements Config
 {
+  /**
+   * Pages that are included
+   * @var string[]
+   */
   private $includedPages = array();
+
+  /**
+   * Pages that are excluded
+   * @var string[]
+   */
   private $excludedPages = array();
 
+  /**
+   * The created tests.
+   * @var array
+   */
   private $testCases = array();
+
+  /**
+   * This flag indicates if this config file should inherit the pages from its
+   * parent.
+   *
+   * @var bool
+   */
   private $inherit = true;
 
+  /**
+   * The directory of the yaml file this configuration was created from
+   * @var string
+   */
   private $baseDir;
 
+  /**
+   * The parent configuration. Used to inherit pages.
+   * @var TestSuite
+   */
   private $parentConfig;
 
+  /**
+   * Set the parent config if needed.
+   *
+   * @param TestSuite $parentConfig
+   */
   public function __construct(TestSuite $parentConfig = null)
   {
     $this->parentConfig = $parentConfig;
   }
 
+  /**
+   * Sets the base dir. This is needed because some tags need the path to the config
+   * entry file.
+   *
+   * @param string $baseDir
+   */
   public function setBaseDir($baseDir)
   {
     $this->baseDir = $baseDir;
   }
 
+  /**
+   * Returns the base directory of the config file.
+   *
+   * @return string
+   */
   public function getBaseDir()
   {
     if (is_null($this->baseDir))
@@ -33,11 +91,21 @@ class TestSuite implements Config
     return $this->baseDir;
   }
 
+  /**
+   * Include an additional page to the config.
+   *
+   * @param string $page
+   */
   public function includePage($page)
   {
     $this->includedPages[$page] = $page;
   }
 
+  /**
+   * Includes an array containing pages to the config.
+   *
+   * @param string[] $pages
+   */
   public function includePages($pages)
   {
     foreach( $pages as $page )
@@ -46,11 +114,21 @@ class TestSuite implements Config
     }
   }
 
+  /**
+   * Removes a page from the config.
+   *
+   * @param string $page
+   */
   public function excludePage($page)
   {
     $this->excludedPages[$page] = $page;
   }
 
+  /**
+   * Removes a set of pages from this config.
+   *
+   * @param string[] $pages
+   */
   public function excludePages($pages)
   {
     foreach( $pages as $page )
@@ -59,18 +137,25 @@ class TestSuite implements Config
     }
   }
 
+  /**
+   * This function is called if this config should not inherit the pages from its parent.
+   */
   public function doNotInherit()
   {
     $this->inherit = false;
   }
 
-  public function clearPages()
-  {
-    $this->includedPages = array();
-    $this->excludedPages = array();
-  }
-
-  public function createTestCase($name, $className, $parameters)
+  /**
+   * This function adds a test to the config and returns a new config connected to the
+   * test.
+   *
+   * @todo we should use the Test class for this
+   *
+   * @param string $name
+   * @param string $className
+   * @param array $parameters
+   */
+  public function createTestCase($name, $className, array $parameters)
   {
     $config = new self($this);
 
@@ -79,6 +164,11 @@ class TestSuite implements Config
     return $config;
   }
 
+  /**
+   * Returns the list of pages.
+   *
+   * @return string[]
+   */
   public function getPages()
   {
     if ($this->inherit && !is_null($this->parentConfig))
@@ -93,6 +183,11 @@ class TestSuite implements Config
     return array_diff($result, $this->excludedPages);
   }
 
+  /**
+   * Returns the tests.
+   *
+   * @return array
+   */
   public function getTestCases()
   {
     return $this->testCases;
