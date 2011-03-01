6f6cd6e6dccb7e0e3a3ad20deb7381de4e00843f
Nils Langner <langner@plan08.de>
2011-03-01 09:37:02 +0100

    Decoumented the config interface

--- src/LiveTest/Config/Config.php
+++ src/LiveTest/Config/Config.php
@@ -1,7 +1,20 @@
 <?php
 
+/*
+ * This file is part of the LiveTest package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace LiveTest\Config;
 
+/**
+ * This interface has to implemeted if a configuration should be created/filled by
+ * the config parser.
+ *
+ * @author Nils Langner
+ */
 interface Config
 {
 
