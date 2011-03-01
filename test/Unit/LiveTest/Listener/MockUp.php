5df0c9a4a4ecb1adfb7f45518bc10a276b3d1775
Nils Langner <langner@plan08.de>
2011-03-01 13:55:03 +0100

    Added unit test for dispatcher, added Listener MockUp

---
+++ test/Unit/LiveTest/Listener/MockUp.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Unit\LiveTest\Listener;
+
+use Annovent\Event\Dispatcher;
+
+use LiveTest\Listener\Listener;
+
+class MockUp implements Listener
+{
+  private $dispatcher;
+  private $runId;
+
+  private $foo;
+
+  public function __construct($runId, Dispatcher $eventDispatcher)
+  {
+    $this->runId = $runId;
+    $this->dispatcher = $eventDispatcher;
+  }
+
+  public function init( $foo )
+  {
+    $this->foo = $foo;
+  }
+
+  public function getFoo( )
+  {
+    return $this->foo;
+  }
+}
