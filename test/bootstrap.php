2760c02c8c61d43824247bcba67a7a208337e74d
Mike Lohmann <mike.h.lohmann@googlemail.com>
2011-03-03 11:55:10 +0100

    removed var_dump

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -10,7 +10,6 @@ function LiveTestUnit_Autoload($classname)
   $currentDir = __DIR__;
   //$classname = str_replace('Test\\', '\\', $classname);
   $classPath = $currentDir . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $classname).'.php';
-  var_dump($classPath);
 
   if ( file_exists( $classPath )) {
     include_once $classPath;
