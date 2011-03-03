22d7d4fcf5c2eb6fe1d9952d6d5e4102613bb367
Mike Lohmann <mike.h.lohmann@googlemail.com>
2011-03-03 21:48:46 +0100

    removed date_set_default_time_zone, because it is part of php.ini. Perhaps later it could become configurable

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,6 +1,4 @@
 <?php
-date_default_timezone_set('Europe/Berlin');
-
 include_once __DIR__.'/../src/bootstrap.php';
 require_once 'Zend/Loader/Autoloader.php';
 Zend_Loader_Autoloader::getInstance();
