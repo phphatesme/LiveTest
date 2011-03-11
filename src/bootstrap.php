67d4b2f67d946a89364affad80b456604736ec26
Nils Langner <langner@plan08.de>
2011-03-11 15:13:48 +0100

    fixed bug in bootloader

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,28 +1,21 @@
 <?php
 
-// -----------------------------------------------------------------------
-//  Include 3rd party dependecies
-// -----------------------------------------------------------------------
-
-include_once 'lib/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-$classLoader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
-
-$classLoader->registerNamespace('Doctrine', __DIR__.'\lib');
-$classLoader->registerNamespace('Symfony' , __DIR__.'\lib');
-$classLoader->registerNamespace('Base'    , __DIR__.'\lib');
-
-// include the Annovent event despatcher
-include_once 'lib/Annovent/bootstrap.php';
-
-// include zend framework
+// include zend framework 1
+use Doctrine\Common\Annotations\AnnotationReader;
 set_include_path(__DIR__ . '/lib' . PATH_SEPARATOR . get_include_path());
 require_once 'Zend/Loader/Autoloader.php';
 Zend_Loader_Autoloader::getInstance();
 
-// -----------------------------------------------------------------------
-//  Include LiveTest classes and functions
-// -----------------------------------------------------------------------
+// include standard namespaces
+include_once 'lib/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+
+$classLoader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
+
+$classLoader->registerNamespace('Doctrine', __DIR__.DIRECTORY_SEPARATOR.'lib');
+$classLoader->registerNamespace('Symfony' , __DIR__.DIRECTORY_SEPARATOR.'lib');
+$classLoader->registerNamespace('Base'    , __DIR__.DIRECTORY_SEPARATOR.'lib');
+$classLoader->registerNamespace('Annovent', __DIR__.DIRECTORY_SEPARATOR.'lib');
 
 $classLoader->registerNamespace('LiveTest', __DIR__);
+
 $classLoader->register();
--- src/lib/Annovent/bootstrap.php
+++ src/lib/Annovent/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-include_once 'Functions.php';
-
 function Annovent_Autoload($classname)
 {
   $currentDir = __DIR__;
