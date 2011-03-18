41ec23003aee0ad83da671e2d5082848ead7cac0
Nils Langner <langner@plan08.de>
2011-03-18 20:39:51 +0100

    Added phmLabs/Statistics

---
+++ src/LiveTest/Packages/phmLabs/Listener/Statistics.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace LiveTest\Packages\phmLabs\Listener;
+
+use LiveTest\TestRun\Information;
+use LiveTest\TestRun\Properties;
+use LiveTest\Listener\Base;
+
+use Base\Http\Client\Zend;
+
+class Statistics extends Base
+{
+  private $urls;
+  private $tests;
+
+  const PHM_API = 'http://www.phmlabs.com/livetest/api/statistics.php';
+
+  /**
+   * @Event("LiveTest.Run.PreRun")
+   * @param Properties $properties
+   */
+  public function preRun(Properties $properties)
+  {
+    $this->urls = count($properties->getTestSets());
+    $this->tests = $this->getTotalTestCount($properties);
+  }
+
+  /**
+   * @param Properties $properties
+   */
+  private function getTotalTestCount(Properties $properties)
+  {
+    $count = 0;
+    foreach ( $properties->getTestSets() as $testSet )
+    {
+      $count += $testSet->getTestCount();
+    }
+    return $count;
+  }
+
+  /**
+   * @Event("LiveTest.Run.PostRun")
+   *
+   * @param Information $information
+   */
+  public function postRun(Information $information)
+  {
+    try
+    {
+      $client = new Zend(self::PHM_API . '?urls=' . $this->urls . '&tests=' . $this->tests);
+      $client->request();
+    }
+    catch (\Exception $e )
+    {
+    }
+  }
+}
