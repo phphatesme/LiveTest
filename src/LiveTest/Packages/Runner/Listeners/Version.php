37966fab4d5d44a0c4a6609e281db9d87fc9bc7c
Nils Langner <langner@plan08.de>
2011-03-21 17:25:33 +0100

    Added --version

--- src/LiveTest/Packages/Runner/Listeners/Version.php
+++ src/LiveTest/Packages/Runner/Listeners/Version.php
@@ -13,22 +13,16 @@ use phmLabs\Components\Annovent\Event\Event;
 use LiveTest\Listener\Base;
 
 /**
- * This extension is used to print the help content if the --help argument is set
- *
  * @author Nils Langner
  */
 class Version extends Base
 {
   /**
-   * This template that contains the help text
-   *
    * @var string The filename. Relative to __DIR__.
    */
   private $template = 'Help/template.tpl';
 
   /**
-   * This function echoes the global help if the --help command line argument is set
-   *
    * @Event("LiveTest.Runner.Init")
    *
    * @param array $arguments
