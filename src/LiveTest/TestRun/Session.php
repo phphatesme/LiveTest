52aad1157335a6dc4b41cc5baf87d2e84c42e97e
develop <develop@ela-webdev.local>
2011-05-13 12:57:40 +0200

    Added cURL check on start

---
+++ src/LiveTest/TestRun/Session.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace LiveTest\TestRun;
+
+use Base\Http\Client\Client;
+
+class TestSession
+{
+  private $testSets = array ();
+  private $client;
+  
+  public function __construct(Client $client)
+  {
+  	$this->client = $client;
+  }
+  
+  public function getClient( )
+  {
+  	return $this->client;
+  }
+  
+  public function addTestSet(TestSet $testSet)
+  {
+    $this->testSets[] = $testSet;
+  }
+  
+  public function getTestSets()
+  {
+    return $this->testSets;
+  }
+}
