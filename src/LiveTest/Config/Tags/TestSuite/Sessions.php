83eac1a56025a1b8191aa143d60ebefcbb847223
Mike Lohmann <mike.h.lohmann@googlemail.com>
2011-05-13 08:33:52 +0200

    First requests programm

---
+++ src/LiveTest/Config/Tags/TestSuite/Sessions.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the LiveTest package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LiveTest\Config\Tags\TestSuite;
+
+/**
+ * This tag adds the test cases to the configuration. All tags that are not known withing this class are
+ * handed to parser.
+ *
+ * @example
+ * Sessions:
+     user_handling:
+       Pages:
+         - /login.php:
+           post:
+             key1: value1
+             key2: value2
+         - /index.php
+         
+ *
+ * @author Mike Lohmann
+ */
+class Sessions extends Base
+{
+  /**
+   * @see LiveTest\Config\Tags\TestSuite.Base::doProcess()
+   */
+  protected function doProcess(\LiveTest\Config\TestSuite $config, array $parameters)
+  {
+    foreach ( $parameters as $name => $value )
+    {
+      echo "\n\n";
+      print_r($name);
+      echo "\n";
+      print_r($value);
+      
+    }
+    die();
+  }
+}
